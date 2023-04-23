a76a5034f5a73f03f51e8f1767dc5e27d4a8bc35
Alon Krymgand <me@alon.kr>
2023-04-23 20:32:41 +0300

    Added user model

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { User } from "$lib/models/user";
+
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Locals {
+			user: User | null
+		}
 		// interface Error {}
-		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
 	}
---
+++ src/lib/models/user.ts
@@ -0,0 +1,10 @@
+export interface InternalUser {
+    name :string;
+    email: string;
+    passwordHash: string;
+}
+
+export interface User {
+    name: string;
+    email: string;
+}
