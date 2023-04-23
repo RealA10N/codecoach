b8bf72102e8d20baf6c1812c4ccc14d0dddc02ad
Alon Krymgand <me@alon.kr>
2023-04-23 13:39:12 +0300

    Added problem group component

---
+++ src/lib/components/ProblemGroup.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import ProblemListing from '$lib/components/ProblemListing.svelte';
+	import type Problem from '$lib/models/Problem';
+
+	export let title: string;
+	export let subtitle: string | null = null;
+	export let problems: Problem[];
+</script>
+
+<div class="my-8">
+	<h2 class="font-bold mt-4 mb-2 text-3xl">
+		{title}
+		{#if subtitle}<span class="font-light text-lg">{subtitle}</span>{/if}
+	</h2>
+
+	<p class="my-2">
+		<slot />
+	</p>
+
+	{#each problems as problem}
+		<ProblemListing {...problem} isAccepted={false} />
+	{/each}
+</div>
