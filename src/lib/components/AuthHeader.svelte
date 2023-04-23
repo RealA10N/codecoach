4b1b5a1db65128637adc030b6ee43e378310fe59
Alon Krymgand <me@alon.kr>
2023-04-23 13:38:34 +0300

    Added auth header

---
+++ src/lib/components/AuthHeader.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Fa from 'svelte-fa';
+	import {
+		faRightToBracket,
+		faRightFromBracket
+	} from '@fortawesome/free-solid-svg-icons';
+
+	let name: string | null;
+</script>
+
+<div class="text-center sm:text-right">
+	{#if name}
+		<a href="/logout" class="inline-block no-underline">
+			Welcome, {name}
+			<Fa icon={faRightFromBracket} fw class="inline-block ml-1" />
+		</a>
+	{:else}
+		<a href="/login" class="inline-block no-underline">
+			<Fa icon={faRightToBracket} fw class="inline-block mr-1" />
+			Login to view progress
+		</a>
+	{/if}
+</div>
