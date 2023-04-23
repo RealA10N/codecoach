6a5446edc842979c92cb8054377f83f1c595b284
Alon Krymgand <me@alon.kr>
2023-04-23 16:45:41 +0300

    Added login route

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,23 @@
+<div class="flex justify-center">
+	<form method="post" class="max-w-sm">
+		<h1 class="text-center mb-6">Welcome back! 👋</h1>
+
+		<label for="email">Email</label>
+		<input type="email" id="email" />
+
+		<label for="password">Password</label>
+		<input type="password" id="password" />
+
+		<div class="text-center">
+			<button
+				class="mt-8 bg-red-400 dark:bg-red-700 py-1 px-5 rounded-full shadow-sm text-neutral-100 inline-block w-32 focus:scale-105 transition hover:bg-red-500 hover:dark:bg-red-600"
+				>Login
+			</button>
+		</div>
+
+		<div class="mt-2 text-center text-sm">
+			First time?
+			<a href="/register">Register here</a>
+		</div>
+	</form>
+</div>
