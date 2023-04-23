c1c6576ce87c89f5b40d8cea5815b5a21f50db13
Alon Krymgand <me@alon.kr>
2023-04-23 11:35:16 +0300

    Added accepted design for problem listing

--- src/lib/components/ProblemListing.svelte
+++ src/lib/components/ProblemListing.svelte
@@ -2,14 +2,25 @@
 	export let title: string;
 	export let subtitle: string;
 	export let url: string;
+	export let isAccepted: boolean = false;
 </script>
 
 <a
-	class="py-1 px-3 no-underline transition-colors cursor-pointer w-full inline-block text-center
-		bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700"
+	class="py-1 px-3 transition-colors w-full inline-block text-center
+		{isAccepted ? 'accepted' : 'not-applicable'}"
 	href={url}
 	target="_blank"
 >
-	<span>{title}</span>
-	<span class="text-neutral-500 text-xs">{subtitle}</span>
+	{title}
+	<span class="text-xs opacity-50">{subtitle}</span>
 </a>
+
+<style lang="postcss">
+	.accepted {
+		@apply bg-green-200 hover:bg-green-300 dark:bg-green-900 dark:hover:bg-green-800;
+	}
+
+	.not-applicable {
+		@apply bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700;
+	}
+</style>
