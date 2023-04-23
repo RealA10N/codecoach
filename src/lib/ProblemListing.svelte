dad04dde5f88666f7346e42b48dbd01814f0d015
Alon Krymgand <me@alon.kr>
2023-04-23 11:11:41 +0300

    Added problem listing component

---
+++ src/lib/ProblemListing.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let title: string;
+	export let subtitle: string;
+	export let url: string;
+</script>
+
+<a
+	class="py-1 px-3 no-underline transition-colors cursor-pointer w-full inline-block text-center
+		bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700"
+	href={url}
+	target="_blank"
+>
+	<span>{title}</span>
+	<span class="text-neutral-500 text-xs">{subtitle}</span>
+</a>
