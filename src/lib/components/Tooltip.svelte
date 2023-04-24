101f7ae0ad00fd890c129e8c0a8bb09d7ba0be40
Alon Krymgand <me@alon.kr>
2023-04-24 19:21:30 +0300

    Added tooltip animation

--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -7,10 +7,10 @@
 		<slot />
 	</span>
 	<span
-		class="opacity-0 peer-hover:opacity-100 delay-300 ease-in-out transition-opacity
-	absolute -translate-y-full left-1/2 -translate-x-1/2
-	dark:bg-neutral-700 bg-neutral-200 rounded-md shadow-md
-	py-1 px-2 select-none min-w-max pointer-events-none"
+		class="opacity-0 peer-hover:opacity-100 delay-300 ease-in-out transition-all
+	absolute -translate-y-3/4 peer-hover:-translate-y-full left-1/2 -translate-x-1/2
+	scale-75 peer-hover:scale-100 py-1 px-2 select-none min-w-max pointer-events-none
+	dark:bg-neutral-700 bg-neutral-200 rounded-md shadow-md"
 	>
 		{text}
 	</span>
