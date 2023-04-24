b11104106e2222192b8b11ebe549150c0191e4b1
Alon Krymgand <me@alon.kr>
2023-04-25 02:31:31 +0300

    Added letf to solve footer

--- src/lib/components/ProblemGroup.svelte
+++ src/lib/components/ProblemGroup.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import ProblemListing from '$lib/components/ProblemListing.svelte';
 	import type Problem from '$lib/models/Problem';
+	import Fa from 'svelte-fa';
+	import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 	export let title: string;
 	export let subtitle: string | null = null;
@@ -20,4 +22,9 @@
 	{#each problems as problem}
 		<ProblemListing {...problem} isAccepted={false} />
 	{/each}
+
+	<p class="text-center my-2 text-sm">
+		<Fa icon={faStar} class="inline opacity-50 hover:rotate-[72deg]" fw />
+		Solve all problems to unlock additional ones!
+	</p>
 </div>
