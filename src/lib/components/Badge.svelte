0aabe4a4d49af82d4eacbce4265625c9eda35e16
Alon Krymgand <me@alon.kr>
2023-04-28 14:55:26 +0300

    Added badge component

---
+++ src/lib/components/Badge.svelte
@@ -0,0 +1,7 @@
+<span
+	class="rounded-full font-mono py-[1] px-1.5 border text-xs align-middle
+    text-neutral-500 border-neutral-500 dark:text-neutral-400 dark:border-neutral-400
+    hover:bg-neutral-500 hover:dark:bg-neutral-400 hover:text-neutral-200 hover:dark:text-neutral-800"
+>
+	<slot />
+</span>
