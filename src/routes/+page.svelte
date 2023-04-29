6120e43f89a965b650aaf3971ea54c58c14d335b
Alon Krymgand <me@alon.kr>
2023-04-29 20:20:21 +0300

    Updated Codeforces broken link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 <ProblemGroup problemGroup={w1} solutions={data.solutions}>
 	Welcome to Competitive Programming! 🎉 In this first training session, you
 	will mainly get to know
-	<a href="https://codeforce.com" target="_blank">Codeforces</a>
+	<a href="https://codeforces.com" target="_blank">Codeforces</a>
 	and <a href="https://cses.fi/problemset" target="_blank">CSES</a>, two popular
 	online judges for competitive programming that we will be using this semester.
 	It is also highly encouraged to solve the problems using C++ and getting used
