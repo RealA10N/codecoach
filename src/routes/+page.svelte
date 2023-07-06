42d5502244d564f66affbd6627b7e5a75d845259
Alon Krymgand <me@alon.kr>
2023-07-06 13:42:04 +0300

    Added problems to a single JSON file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,13 @@
 	import ProblemGroup from '$lib/components/ProblemGroup.svelte';
 	import AuthHeader from '$src/lib/components/AuthHeader.svelte';
 	import type { PageServerData } from './$types';
-	import w0 from '$src/problems/week0.json';
-	import w1 from '$src/problems/week1.json';
+	import problems from '$src/problems.json';
 
 	export let data: PageServerData;
 </script>
 
 <AuthHeader user={data.loggedInUser} />
 
-<ProblemGroup problemGroup={w0} solutions={data.solutions} />
-<ProblemGroup problemGroup={w1} solutions={data.solutions} />
+{#each problems as problem}
+	<ProblemGroup problemGroup={problem} solutions={data.solutions} />
+{/each}
