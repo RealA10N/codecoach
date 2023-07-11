b840b5a912d9291c287bf14c4b46b8e00c66ccec
Alon Krymgand <me@alon.kr>
2023-07-11 21:17:36 +0300

    Added external code snippet component

---
+++ src/lib/components/snippet/ExternalCodeSnippet.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import CodeSnippet from './CodeSnippet.svelte';
+
+	export let url: string;
+	export let language: string;
+
+	let source: string;
+
+	onMount(async () => {
+		fetch(url)
+			.then(async (response) => response.text())
+			.then((data) => {
+				source = data;
+				console.log(source);
+			});
+	});
+</script>
+
+<CodeSnippet {source} {language} />
