44636c975805bc59c8a2cf3ca86eddfdb9e68e7d
Alon Krymgand <me@alon.kr>
2023-07-13 22:32:51 +0300

    Code snippet not position absolutely

--- src/lib/components/model/Model.svelte
+++ src/lib/components/model/Model.svelte
@@ -13,9 +13,10 @@
 	bind:this={dialog}
 	on:close={() => (showModel = false)}
 	on:click|self={() => dialog.close()}
-	class="bg-neutral-800 shadow-lg absolute m-auto w-full max-w-min
-		backdrop:backdrop-blur-[2px] backdrop:bg-black backdrop:bg-opacity-50
-		open:animate-pop-in text-neutral-200"
+	class="bg-neutral-800 shadow-sm m-4 w-100 fixed max-h-[75vh]
+			top-1/2 left-1/2 box-border -translate-x-1/2 -translate-y-1/2
+			backdrop:backdrop-blur-[2px] backdrop:bg-black backdrop:bg-opacity-50
+			open:animate-pop-in text-neutral-200"
 	{...$$restProps}
 >
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
