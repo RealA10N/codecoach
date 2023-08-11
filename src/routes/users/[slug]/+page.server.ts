ffa0f90b5d3d16f5f4326c4ae5a98c6c95adbb68
Alon Krymgand <me@alon.kr>
2023-08-11 20:55:53 +0300

    Added redirect form non existing user

--- src/routes/users/[slug]/+page.server.ts
+++ src/routes/users/[slug]/+page.server.ts
@@ -1,8 +1,10 @@
 import { getUserIntegrations, getUser } from "$lib/services/db.server"
+import { redirect } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
 
 export const load: PageServerLoad = async ({ locals, params }) => {
     const user = await getUser(locals.db, params.slug)
     const integrations = user ? await getUserIntegrations(locals.db, user) : null
+    if (user === null || integrations === null) throw redirect(302, '/users')
     return {user, integrations}
 }
