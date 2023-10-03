9e5e20bd970e2551190ea42cc6d044c3d1d77ca0
Alon Krymgand <me@alon.kr>
2023-10-03 16:30:25 +0300

    Added custom page for users with no integrations

--- src/routes/users/[slug]/+page.svelte
+++ src/routes/users/[slug]/+page.svelte
@@ -11,26 +11,37 @@
 	{data.user?.name}
 </h2>
 
-<div class="inline-flex gap-1">
-	{#each data.integrations ?? [] as integration}
-		<AnyIntegrationLink {integration} />
-	{/each}
-
-	{#if data.isLoggedUser}
-		<button class="align-middle px-2 py-1 no-underline cursor-pointer">
-			<!-- TODO: add onclick action! -->
-			<Fa icon={faEdit} class="inline-block" /> Edit
-		</button>
-	{/if}
-</div>
-
-<div class="bg-neutral-200 dark:bg-neutral-800 my-2 p-2 rounded-lg">
-	<div class="flex-wrap justify-around flex leading-none md:">
-		{#each data.statPanels as panel}
-			<div class="m-2">
-				<h3 class="m-0">{panel.stat} {panel.units}</h3>
-				<span class="text-sm">{panel.info}</span>
-			</div>
-		{/each}
+{#if data.user.integrations.length === 0}
+	<div
+		class="bg-neutral-200 dark:bg-neutral-800 my-2 p-4 rounded-lg leading-none text-center"
+	>
+		<h3 class="m-0">No data available</h3>
+		<p class="mx-4">
+			The user has not yet connected any third party judge to his account.
+		</p>
 	</div>
-</div>
+{:else}
+	<div class="inline-flex gap-1">
+		{#each data.integrations ?? [] as integration}
+			<AnyIntegrationLink {integration} />
+		{/each}
+
+		{#if data.isLoggedUser}
+			<button class="align-middle px-2 py-1 no-underline cursor-pointer">
+				<!-- TODO: add onclick action! -->
+				<Fa icon={faEdit} class="inline-block" /> Edit
+			</button>
+		{/if}
+	</div>
+
+	<div class="bg-neutral-200 dark:bg-neutral-800 my-2 p-2 rounded-lg">
+		<div class="flex-wrap justify-around flex leading-none md:">
+			{#each data.statPanels as panel}
+				<div class="m-2">
+					<h3 class="m-0">{panel.stat} {panel.units}</h3>
+					<span class="text-sm">{panel.info}</span>
+				</div>
+			{/each}
+		</div>
+	</div>
+{/if}
