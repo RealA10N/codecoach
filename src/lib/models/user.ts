3fa8aaff5cc792982d611f38d2dfa2a82b65d29b
Alon Krymgand <me@alon.kr>
2023-10-03 04:05:55 +0300

    Formatting

--- src/lib/models/user.ts
+++ src/lib/models/user.ts
@@ -1,25 +1,25 @@
-import { CCBaseModel, ModelId } from "$lib/models/base"
-import { z } from "zod";
+import { CCBaseModel, ModelId } from '$lib/models/base';
+import { z } from 'zod';
 
-export const UserNameModel = z.string().min(3)
-export type UserName = z.infer<typeof UserNameModel>
+export const UserNameModel = z.string().min(3);
+export type UserName = z.infer<typeof UserNameModel>;
 
-export const UserEmailModel = z.string().email()
-export type UserEmail = z.infer<typeof UserEmailModel>
+export const UserEmailModel = z.string().email();
+export type UserEmail = z.infer<typeof UserEmailModel>;
 
 export const UserModel = CCBaseModel.extend({
-  name: UserNameModel,
-  email: UserEmailModel,
-  integrations: z.array(ModelId),
-})
+	name: UserNameModel,
+	email: UserEmailModel,
+	integrations: z.array(ModelId)
+});
 
-export type User = z.infer<typeof UserModel>
+export type User = z.infer<typeof UserModel>;
 
-export const PasswordHashModel = z.string().length(60)
-export type PasswordHash = z.infer<typeof PasswordHashModel>
+export const PasswordHashModel = z.string().length(60);
+export type PasswordHash = z.infer<typeof PasswordHashModel>;
 
 export const UserPasswordModel = CCBaseModel.extend({
-  password_hash: PasswordHashModel
-})
+	password_hash: PasswordHashModel
+});
 
-export type UserPassword = z.infer<typeof UserPasswordModel>
+export type UserPassword = z.infer<typeof UserPasswordModel>;
