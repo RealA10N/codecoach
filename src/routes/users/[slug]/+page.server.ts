2fc85b49bb301a6e7a14457db99e5bed8c9706eb
Alon Krymgand <me@alon.kr>
2023-10-03 20:12:46 +0300

    Optimization: DB now counts submissions

--- src/routes/users/[slug]/+page.server.ts
+++ src/routes/users/[slug]/+page.server.ts
@@ -1,12 +1,12 @@
 import {
 	getUserIntegrations,
 	getUser,
-	getUserSubmissions
+	type DatabaseContainers
 } from '$lib/services/db.server';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import type { User } from '$lib/models/user';
-import { SubmissionVerdict, type Submission } from '$lib/models/submission';
+import { SubmissionVerdict } from '$lib/models/submission';
 
 type statPanel = {
 	stat: number;
@@ -19,37 +19,89 @@ export const load: PageServerLoad = async ({ locals, params }) => {
 	if (!user) throw redirect(302, '/users');
 	const integrations = user ? await getUserIntegrations(locals.db, user) : null;
 	const isLoggedUser = locals.loggedInUser?.id == user?.id;
-	const statPanels = await getStatPanels(
-		(await getUserSubmissions(locals.db, user)) ?? []
-	);
+	const statPanels = await getStatPanels(locals.db, user);
 	return { user, integrations, isLoggedUser, statPanels };
 };
 
-const getStatPanels = async (submissions: Submission[]) => {
-	const panels: statPanel[] = [];
+const getStatPanels = async (
+	db: DatabaseContainers,
+	user: User
+): Promise<statPanel[]> => {
+	const [submissionsCount, acceptedSubmissionsCount, solvedProblemsCount] =
+		await Promise.all([
+			getSubmissionsCount(db, user),
+			getAcceptedSubmissionsCount(db, user),
+			getSolvedProblemsCount(db, user)
+		]);
 
-	panels.push({
-		stat: submissions.length,
-		units: 'Submissions',
-		info: 'Submitted all time'
-	});
+	return [
+		{
+			stat: submissionsCount,
+			units: 'Submissions',
+			info: 'Submitted all time'
+		},
+		{
+			stat: acceptedSubmissionsCount,
+			units: 'Submissions',
+			info: 'Accepted all time'
+		},
+		{
+			stat: solvedProblemsCount,
+			units: 'Problems',
+			info: 'Solved all time'
+		}
+	];
+};
 
-	const acceptedSubmissions = submissions.filter(
-		(submission) => submission.verdict == SubmissionVerdict.Enum.accepted
-	);
-	panels.push({
-		stat: acceptedSubmissions.length,
-		units: 'Submissions',
-		info: 'Accepted all time'
-	});
+const getSubmissionsCount = async (
+	db: DatabaseContainers,
+	user: User
+): Promise<number> => {
+	const { resources } = await db.submissions.items
+		.query({
+			query: `SELECT VALUE COUNT(1) FROM c
+			WHERE ARRAY_CONTAINS(@integrationIds, c.integration.id)`,
+			parameters: [{ name: '@integrationIds', value: user.integrations }]
+		})
+		.fetchAll();
+	return resources[0] ?? 0;
+};
 
-	panels.push({
-		stat: new Set(
-			acceptedSubmissions.map((submissions) => submissions.problem.id)
-		).size,
-		units: 'Problems',
-		info: 'Solved all time'
-	});
+const getAcceptedSubmissionsCount = async (
+	db: DatabaseContainers,
+	user: User
+): Promise<number> => {
+	const { resources } = await db.submissions.items
+		.query({
+			query: `SELECT VALUE COUNT(1) FROM c
+					WHERE ARRAY_CONTAINS(@integrationIds, c.integration.id)
+					AND c.verdict = @accepted`,
+			parameters: [
+				{ name: '@integrationIds', value: user.integrations },
+				{ name: '@accepted', value: SubmissionVerdict.Enum.accepted }
+			]
+		})
+		.fetchAll();
+	return resources[0] ?? 0;
+};
 
-	return panels;
+const getSolvedProblemsCount = async (
+	db: DatabaseContainers,
+	user: User
+): Promise<number> => {
+	const { resources } = await db.submissions.items
+		.query({
+			query: `SELECT VALUE COUNT(1) FROM (
+				SELECT DISTINCT c.problem.id
+				FROM c
+				WHERE c.verdict = @accepted
+				AND ARRAY_CONTAINS(@integrationIds, c.integration.id)
+			)`,
+			parameters: [
+				{ name: '@integrationIds', value: user.integrations },
+				{ name: '@accepted', value: SubmissionVerdict.Enum.accepted }
+			]
+		})
+		.fetchAll();
+	return resources[0] ?? 0;
 };
