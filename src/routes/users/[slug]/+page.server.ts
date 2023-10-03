c0470c9f69d9f29b0aac972037b2ebd6f72ba6d6
Alon Krymgand <me@alon.kr>
2023-10-03 15:45:44 +0300

    Added stats to user page

--- src/routes/users/[slug]/+page.server.ts
+++ src/routes/users/[slug]/+page.server.ts
@@ -1,11 +1,55 @@
-import { getUserIntegrations, getUser } from '$lib/services/db.server';
+import {
+	getUserIntegrations,
+	getUser,
+	getUserSubmissions
+} from '$lib/services/db.server';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import type { User } from '$lib/models/user';
+import { SubmissionVerdict, type Submission } from '$lib/models/submission';
+
+type statPanel = {
+	stat: number;
+	units: string;
+	info: string;
+};
 
 export const load: PageServerLoad = async ({ locals, params }) => {
 	const user = await getUser(locals.db, params.slug);
 	if (!user) throw redirect(302, '/users');
 	const integrations = user ? await getUserIntegrations(locals.db, user) : null;
 	const isLoggedUser = locals.loggedInUser?.id == user?.id;
-	return { user, integrations, isLoggedUser };
+	const statPanels = await getStatPanels(
+		(await getUserSubmissions(locals.db, user)) ?? []
+	);
+	return { user, integrations, isLoggedUser, statPanels };
+};
+
+const getStatPanels = async (submissions: Submission[]) => {
+	const panels: statPanel[] = [];
+
+	panels.push({
+		stat: submissions.length,
+		units: 'Submissions',
+		info: 'Submitted all time'
+	});
+
+	const acceptedSubmissions = submissions.filter(
+		(submission) => submission.verdict == SubmissionVerdict.Enum.accepted
+	);
+	panels.push({
+		stat: acceptedSubmissions.length,
+		units: 'Submissions',
+		info: 'Accepted all time'
+	});
+
+	panels.push({
+		stat: new Set(
+			acceptedSubmissions.map((submissions) => submissions.problem.id)
+		).size,
+		units: 'Problems',
+		info: 'Solved all time'
+	});
+
+	return panels;
 };
--- src/routes/users/[slug]/+page.svelte
+++ src/routes/users/[slug]/+page.svelte
@@ -18,8 +18,19 @@
 
 	{#if data.isLoggedUser}
 		<button class="align-middle px-2 py-1 no-underline cursor-pointer">
-			<!-- TODO: add onclick action!-->
+			<!-- TODO: add onclick action! -->
 			<Fa icon={faEdit} class="inline-block" /> Edit
 		</button>
 	{/if}
+</div>
+
+<div class="bg-neutral-200 dark:bg-neutral-800 my-2 p-2 rounded-lg">
+	<div class="flex-wrap justify-around flex leading-none md:">
+		{#each data.statPanels as panel}
+			<div class="m-2">
+				<h3 class="m-0">{panel.stat} {panel.units}</h3>
+				<span class="text-sm">{panel.info}</span>
+			</div>
+		{/each}
+	</div>
 </div>
